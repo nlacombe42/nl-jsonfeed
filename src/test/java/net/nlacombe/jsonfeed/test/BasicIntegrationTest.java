054e68f728d1d3bd71c765985714b6e4e258f26e
Nicolas Lacombe <nicolas.m.lacombe@gmail.com>
2020-11-15 14:06:41 -0500

    downgraded to java 10 so that project running on older java versions can use this lib

--- src/test/java/net/nlacombe/jsonfeed/test/BasicIntegrationTest.java
+++ src/test/java/net/nlacombe/jsonfeed/test/BasicIntegrationTest.java
@@ -10,9 +10,7 @@ public class BasicIntegrationTest {
 
     @Test
     public void serialize_json_feed_with_only_version_title_and_no_items_to_json_text() {
-        var expectedJsonText = """
-            {"version":"https://jsonfeed.org/version/1.1","title":"My Example Feed","items":[]}\
-            """;
+        var expectedJsonText = "{\"version\":\"https://jsonfeed.org/version/1.1\",\"title\":\"My Example Feed\",\"items\":[]}";
         var jsonFeed = JsonFeed.builder(JsonFeedVersion.VERSION_1_1, "My Example Feed").build();
         var json = jsonFeed.toJson();
 
